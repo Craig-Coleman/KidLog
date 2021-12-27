97ede991d1f88846daf39f96dca26ca32269d460
Craig <craig.coleman.cc@gmail.com>
2021-12-27 16:31:16 -0600

    Added render student list to DOM

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 //Fetch Student Roster from API
+document.addEventListener('DOMContentLoaded', getStudents())
 function getStudents() {
     fetch('http://localhost:3000/students')
     .then(res => res.json())
-    .then(students => console.log(students))
+    .then(students => appendStudents(students))
 }
-getStudents()
 
+function appendStudents(students) {
+    for (let student of students) {
+        let studentObj = {
+            id: student.id,
+            name: student.first + ' ' + student.last,
+            grade: student.grade
+        }
+        let studentLine = document.createElement('li');
+        studentLine.textContent = studentObj.name + ` ${studentObj.grade}th`;
+        let studentHolder = document.querySelector('#nameblock');
+        studentHolder.appendChild(studentLine);
+    }
+}
 
 
 
