13dc4013a18d736a93b14c654227806ecb2b01ee
Craig <craig.coleman.cc@gmail.com>
2021-12-29 15:01:34 -0600

    Add post to new student function to persist new student info

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const studentHolder = document.querySelector('#nameblock');
 const studentList = document.createElement('ol');
 
 
-
 function getStudents() {
     fetch('http://localhost:3000/students')
     .then(res => res.json())
@@ -27,7 +26,6 @@ function appendStudent(student) {
     studentHolder.appendChild(studentList);
 
 
-
 const form = document.querySelector('#form');
 form.addEventListener('submit', addNewStudent);
 function addNewStudent(event) {
@@ -38,6 +36,15 @@ function addNewStudent(event) {
         grade: document.querySelector('#grade').value
     };
     appendStudent(studentObj);
+    fetch('http://localhost:3000/students', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(studentObj)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
     form.reset();
 }
 
